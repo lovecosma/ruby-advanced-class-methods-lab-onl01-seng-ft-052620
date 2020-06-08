459650d4dc5cf0e326fcc43898b8d7673a889b99
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-08 16:00:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+# def initialize(name, artist_name)
+#   @name = name
+#   @artist_name = artist_name
+# end
+
 
   def self.all
     @@all
@@ -9,5 +14,31 @@ class Song
   def save
     self.class.all << self
   end
+
+
+def self.create
+song = Song.new
+song.save
+return song
+end
+
+def self.new_by_name(title)
+song = Song.new
+song.name = title
+return song
+end
+
+def self.create_by_name(title)
+  song = Song.new
+  song.name = title
+  song.save
+  return song
+end
+
+ def find_by_name(title)
+   @@all.each do |song|
+     return song if title == song.name 
+   end
+ end
 
 end
